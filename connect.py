a1fbee090a31b3066a1f0ad2c3e3212beb419dd1
Joseph MT <josephalan_4@hotmail.com>
2022-05-13 03:47:05 -0500

    prueba

--- connect.py
+++ connect.py
@@ -94,6 +94,7 @@ def Get_max_song(p_id_user):
     conn = psycopg2.connect(**params)
     cursor = conn.cursor()
     cursor.execute("Select max_song from public.users where id_user ='"+p_id_user+"'")
+    print(cursor)
     return cursor
 
 def Get_post_author(p_id_user):
